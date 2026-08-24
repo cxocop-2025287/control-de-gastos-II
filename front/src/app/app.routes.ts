6da711a7bec4b71c5eb7dbb63e508e59eda241c9
cxocop-2025287 <cxocop-2025287@kinal.edu.gt>
2026-08-23 22:46:59 -0600

    feat: rutas de Angular y configuracion de la app

---
+++ front/src/app/app.routes.ts
@@ -0,0 +1,25 @@
+import { Routes } from '@angular/router';
+import { AuthGuard } from './guards/auth.guard';
+
+export const routes: Routes = [
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./pages/login/login').then((m) => m.LoginComponent),
+  },
+  {
+    path: 'app',
+    canActivate: [AuthGuard],
+    loadComponent: () =>
+      import('./pages/dashboard/dashboard').then((m) => m.DashboardComponent),
+  },
+  {
+    path: '',
+    redirectTo: 'app',
+    pathMatch: 'full',
+  },
+  {
+    path: '**',
+    redirectTo: 'app',
+  },
+];
