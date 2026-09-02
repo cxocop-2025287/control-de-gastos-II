9843835d3a0695f24769794ded24784277179c0a
cxocop-2025287 <cxocop-2025287@kinal.edu.gt>
2026-09-01 21:21:51 -0600

    chore: registrar todas las nuevas rutas en el enrutador principal

--- back/src/routes/index.ts
+++ back/src/routes/index.ts
@@ -1,8 +1,14 @@
 import { Router } from 'express';
 import authRoutes from './auth.routes';
+import incomeRoutes from './income.routes';
+import configRoutes from './config.routes';
+import activityRoutes from './activity.routes';
 
 const router = Router();
 
 router.use('/auth', authRoutes);
+router.use('/incomes', incomeRoutes);
+router.use('/config', configRoutes);
+router.use('/activity', activityRoutes);
 
 export default router;
